2553260a76b60b6c06beea25cb24d88817eca51a
ross-cutts <Ross.cutts@gmail.com>
2024-02-02 11:41:26 -0500

    Update extract_geotechnical_elements.py

--- extract_geotechnical_elements.py
+++ extract_geotechnical_elements.py
@@ -1,9 +1,12 @@
+!pip install xmlschema
 from lxml import etree
 import xmlschema
 import os
 
 # Load the XML file
-file_path = '/content/600304_20180702.xml'
+file_path = '/content/Geosetta_29-01-2024-16-49-35_diggs.xml'
+xsd_file_path= '/content/Geotechnical (1).xsd'
+
 
 # Parse the XML file
 tree = etree.parse(file_path)
@@ -16,11 +19,18 @@ namespaces = {'diggs_geo': 'http://diggsml.org/schemas/2.5.a'}  # Replace with t
 def get_unique_diggs_geo_types(root, namespaces):
     unique_types = set()
     for element in root.iter():
-        # Ensure element.tag is a string before using startswith
+        # Check if element.tag is a string
         if isinstance(element.tag, str) and element.tag.startswith('{'+namespaces['diggs_geo']):
             unique_types.add(etree.QName(element).localname)
     return unique_types
 
+def extract_data_types(xsd_file_path, namespaces):
+    ns = {'xs': 'http://www.w3.org/2001/XMLSchema'}
+    tree = etree.parse(xsd_file_path)
+    root = tree.getroot()
+    complex_types = root.findall('.//xs:complexType', namespaces=ns)
+    complex_type_names = [type.get('name') for type in complex_types if type.get('name')]
+    return complex_type_names
 
 # Extract and print unique diggs_geo types
 unique_diggs_geo_types = get_unique_diggs_geo_types(root, namespaces)
@@ -33,11 +43,24 @@ for dtype in unique_diggs_geo_types:
     diggs_geo_type_list.append(dtype)
 
 # Now, diggs_geo_type_list contains all unique diggs_geo types
-print(diggs_geo_type_list)
 
-# Convert both lists to sets
-set_complex_type_names = set(complex_type_names)
+
+
+# Extract complex types from XSD
+complex_type_names = extract_data_types(xsd_file_path, namespaces)
+
+
+
+# Assuming complex_type_names is your original list
+modified_complex_type_names = [name.rstrip('Type') if name.endswith('Type') else name for name in complex_type_names]
+
+# Convert the modified list to a set
+set_complex_type_names = set(modified_complex_type_names)
+
 set_diggs_geo_type_list = set(diggs_geo_type_list)
+
+print(set_complex_type_names)#.xsd file
+print(set_diggs_geo_type_list)#.diggs file
 
 # Find common elements
 common_values = set_complex_type_names.intersection(set_diggs_geo_type_list)
@@ -49,5 +72,4 @@ common_values_list = list(common_values)
 print("Common elements between complex_type_names and diggs_geo_type_list:")
 for val in common_values_list:
     print(val)
-
 
